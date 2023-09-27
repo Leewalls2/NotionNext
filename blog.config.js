2a5780933dc3f9cca59dbd3f40dc6dd961323d4c
Leewalls2 <109414441+Leewalls2@users.noreply.github.com>
2023-09-27 23:59:07 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -18,7 +18,7 @@ const BLOG = {
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Leewalls', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '产品经理、连续创业者', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || '产品经理', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.leewalls.space', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客,个人', // 网站关键词 英文逗号隔开
 
